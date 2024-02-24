73293aa3d6558531ccbaea751ee34cd9bada2bf3
Vlad Liashenko <107635535+Vladyslav-Liashenko@users.noreply.github.com>
2024-02-24 19:44:57 +0200

    save

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,23 @@
 import { ContactForm } from './ContactForm/ContactForm';
 import { SearchBox } from './SearchBox/SearchBox';
 import { ContactList } from './ContactList/ContactList';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchContacts } from '../redux/contactSlice';
 
 function App() {
+
+  const dispatch = useDispatch();
+  const { loading, error } =  useSelector(state => state.contacts)
+
+  useEffect(() => {
+    dispatch(fetchContacts())
+  }, [dispatch]);
+
   return (
     <>
+      {loading && <p>Loading</p>}
+      {error && <p>Error</p>}
       <h1>Phonebook</h1>
       <ContactForm/>
       <SearchBox/>
--- src/redux/contactSlice.js
+++ src/redux/contactSlice.js
@@ -1,22 +1,49 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
+import axios from 'axios';
 
 const contactSlice = createSlice({
   name: 'contacts',
   initialState: {
     items: [],
+    loading: false,
+    error: false,
   },
   reducers: {
     addContact: (state, { payload }) => {
       state.items.push(payload);
     },
     deleteContact: (state, action) => {
-      const index = state.items.findIndex(contact => contact.id === action.payload);
+      const index = state.items.findIndex(
+        contact => contact.id === action.payload
+      );
       state.items.splice(index, 1);
     },
   },
+  extraReducers: builder =>
+    builder
+      .addCase(fetchContacts.pending, state => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(fetchContacts.fulfilled, (state, {payload}) => {
+        state.loading = false;
+        state.items = payload;
+      })
+      .addCase(fetchContacts.rejected, state => {
+        state.loading = false;
+        state.error = true;
+      }),
 });
+
+export const fetchContacts = createAsyncThunk('contacts/fetch', async () => {
+  const response = await axios.get(
+    'https://65d9c241bcc50200fcdc0d3c.mockapi.io/contacts'
+  );
+  return response.data;
+});
+console.dir(fetchContacts);
 
 export const { addContact, deleteContact } = contactSlice.actions;
 
