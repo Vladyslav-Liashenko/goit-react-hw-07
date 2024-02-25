bd83df213207c2bc1eea9f5cae9896ec295472b3
Vlad Liashenko <107635535+Vladyslav-Liashenko@users.noreply.github.com>
2024-02-25 15:59:25 +0200

    save

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,15 +1,20 @@
+import { createSelector } from '@reduxjs/toolkit';
 export const selectContacts = state => state.contacts.items;
 export const selectFilter = state => state.filter.value;
 export const selectIsLoading = state => state.contacts.loading;
 export const selectError = state => state.contacts.error;
 
-export const selectVisibleContacts = state => {
-  const contacts = selectContacts(state);
-  const filter = selectFilter(state);
-  if (!filter) {
-    return contacts;
+export const contacts = state => state.contacts.items;
+export const filter = state => state.filter.value;
+export const selectVisibleContacts = createSelector(
+  [selectContacts, selectFilter],
+  (contacts, filter) => {
+    console.log(selectVisibleContacts);
+    if (!filter) {
+      return contacts;
+    }
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter)
+    );
   }
-  return contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter)
-  );
-};
+);
