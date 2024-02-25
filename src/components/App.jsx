d525c19b257b69a43c98868a8275b596a6ac184a
Vlad Liashenko <107635535+Vladyslav-Liashenko@users.noreply.github.com>
2024-02-25 14:23:27 +0200

    save loading

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,10 +4,13 @@ import { ContactList } from './ContactList/ContactList';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchContacts, addContact, deleteContact } from '../redux/operations';
+import { selectIsLoading, selectError } from '../redux/selectors';
 
 function App() {
   const dispatch = useDispatch();
-  const { loading, error } = useSelector(state => state.contacts);
+  // const { loading, error } = useSelector(state => state.contacts);
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
 
   useEffect(() => {
     dispatch(fetchContacts(), addContact(), deleteContact());
@@ -15,8 +18,7 @@ function App() {
 
   return (
     <>
-      {loading && <p>Loading</p>}
-      {error && <p>Error</p>}
+      {isLoading && !error && <b>Loading...</b>}
       <h1>Phonebook</h1>
       <ContactForm />
       <SearchBox />
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,5 @@
 export const selectContacts = (state) => state.contacts.items;
 export const selectFilter = (state) => state.filter.value; 
-
+export const selectIsLoading = (state) => state.contacts.loading;
+export const selectError = state => state.contacts.error;
+ 
